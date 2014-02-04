aa44aaaa722207beded249a7988903b50e10f202
Hyuntae Son <anyjava.kr@gmail.com>
2014-02-05 00:34:00 +0900

    Singleton Pattern

---
+++ src/SingletonMain.java
@@ -0,0 +1,15 @@
+
+public class SingletonMain {
+
+	public static void main( String[] args ) {
+		System.out.println("Start.");
+		Singleton sgt1 = Singleton.getInstance();
+		Singleton sgt2 = Singleton.getInstance();
+		if( sgt1 == sgt2 )
+			System.out.println("sgt1 과 sgt2 는 같은 객체입니다.");
+		else
+			System.out.println("sgt1 과 sgt2 는 다른 객체입니다.");
+		
+		 System.out.println("End.");
+	}
+}
